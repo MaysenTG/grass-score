633c941840255787f5d153f324b4b4f50b42ec62
MaysenAtHSO <maysen.greenwood@henryscheinone.co.nz>
2024-09-10 00:39:15 +1200

    Change numbers to have K, so $100,000 becomes $100K

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def number_to_currency(number)
+    is_negative = number < 0
+    absolute_number = number.abs
+
+    if absolute_number >= 1000
+      abbreviated_number = (absolute_number / 1000).to_i
+      formatted_number = "#{abbreviated_number}K"
+    else
+      formatted_number = super(absolute_number)
+    end
+
+    is_negative ? "-#{formatted_number}" : formatted_number
+  end
 end
