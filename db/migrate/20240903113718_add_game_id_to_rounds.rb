6c473c3ebe943b90fb9585ac7bb8da8ac5ee7315
MaysenAtHSO <maysen.greenwood@henryscheinone.co.nz>
2024-09-12 18:29:06 +1200

    Create docker compose file, update migration to not fail on new DBs

--- db/migrate/20240903113718_add_game_id_to_rounds.rb
+++ db/migrate/20240903113718_add_game_id_to_rounds.rb
@@ -1,5 +1,9 @@
 class AddGameIdToRounds < ActiveRecord::Migration[7.2]
-  def change
-    add_reference :rounds, :game, null: false, foreign_key: true
+  def up
+    add_reference :rounds, :game, null: false, foreign_key: true unless column_exists? :rounds, :game_id
+  end
+
+  def down
+    remove_reference :rounds, :game, index: true if column_exists? :rounds, :game_id
   end
 end
