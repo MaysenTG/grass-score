70f9ab18f1332ab4a27b3ba64ce386fe0d39ef9e
MaysenAtHSO <maysen.greenwood@henryscheinone.co.nz>
2024-12-05 22:31:38 +1300

    Test GH action summaries for RSpec failures

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,31 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { is_expected.to belong_to(:game) }
     it { is_expected.to belong_to(:user).optional }
     it { is_expected.to have_many(:scores).dependent(:destroy) }
-    it { is_expected.to have_many(:winning_rounds).class_name("Round").with_foreign_key("winner_id").dependent(:nullify) }
+    it {
+      is_expected.to have_many(:winning_rounds).class_name('Round').with_foreign_key('winner_id').dependent(:nullify)
+    }
   end
 
-  describe "name" do
-    context "when the player is linked to a user" do
+  describe 'name' do
+    context 'when the player is linked to a user' do
       let(:user) { create(:user) }
-      let(:player) { create(:player, user: user) }
+      let(:player) { create(:player, user:) }
 
       it "returns the user's name" do
-        expect(player.name).to eq(user.name)
+        expect(player.name).to eq(user.name + ' oops')
       end
     end
   end
 
-  describe "validations" do
-    context "when the player is in an unfinished game" do
+  describe 'validations' do
+    context 'when the player is in an unfinished game' do
       let(:game) { create(:game) }
 
       before do
-        create_list(:player, 2, game: game)
-        create(:round, game: game, finished: false)
+        create_list(:player, 2, game:)
+        create(:round, game:, finished: false)
       end
     end
   end
